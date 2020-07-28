04ec65ece555e403d9df7620d57794f61127871b
Enngage <ennspassion@gmail.com>
2020-07-28 09:39:57 +0200

    release

--- lib/sdk-info.generated.ts
+++ lib/sdk-info.generated.ts
@@ -2,6 +2,6 @@
 import { ISDKInfo } from '@kentico/kontent-core';
 export const sdkInfo: ISDKInfo = {
     host: 'npmjs.com',
-    version: '0.0.4',
+    version: '0.0.5',
     name: '@kentico/kontent-recommendations'
 };
